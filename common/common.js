fbd637def0664122e4d58e8b346b394ebf2108ce
YUKI Hiroshi <shimoda@clear-code.com>
2017-08-01 14:38:15 +0900

    Add codes to load configs

---
+++ common/common.js
@@ -0,0 +1,23 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+var configs;
+
+function log(aMessage, ...aArgs)
+{
+	if (!configs || !configs.debug)
+		return;
+
+	console.log('ieview-we: ' + aMessage, ...aArgs);
+}
+
+configs = new Configs({
+	ieapp        : '',
+	ieargs       : '',
+	forceielist  : '',
+	disableForce : false,
+	debug        : false
+});
