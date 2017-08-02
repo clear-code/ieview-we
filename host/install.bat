45dc30eff13a1ab3c49dd5a2dce478f045ac9cce
YUKI Hiroshi <shimoda@clear-code.com>
2017-08-02 12:43:21 +0900

    Show more messages while installation

--- host/install.bat
+++ host/install.bat
@@ -1,16 +1,24 @@
 @ECHO OFF
 
 SET NAME=com.clear_code.ieview_we_host
+
+ECHO Installing %NAME%...
+
 SET INSTALL_DIR=%LocalAppData%\%NAME%
 MD "%INSTALL_DIR%"
 IF %PROCESSOR_ARCHITECTURE% == AMD64 (
+  ECHO Copying binary for AMD64...
   COPY amd64\*.exe "%INSTALL_DIR%\"
 ) ELSE (
+  ECHO Copying binary for x86...
   COPY 386\*.exe "%INSTALL_DIR%\"
 )
 COPY *.json "%INSTALL_DIR%\"
 COPY *.bat "%INSTALL_DIR%\"
 
+ECHO Registering...
 FOR %%f IN ("%INSTALL_DIR%") DO SET EXPANDED_PATH=%%~sf
 REG ADD "HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\%NAME%" /ve /t REG_SZ /d "%EXPANDED_PATH%\%NAME%.json" /f
 
+ECHO Done.
+PAUSE
--- host/uninstall.bat
+++ host/uninstall.bat
@@ -2,7 +2,11 @@
 
 SET NAME=com.clear_code.ieview_we_host
 
+ECHO Uninstalling %NAME%...
+
 REG DELETE "HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\%NAME%" /f
 
 RMDIR /Q /S "%LocalAppData%\%NAME%"
 
+ECHO Done.
+PAUSE
