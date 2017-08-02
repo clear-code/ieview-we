465e87239a149803513d204a02f87f4346a090dd
YUKI Hiroshi <shimoda@clear-code.com>
2017-08-02 12:39:02 +0900

    Install suitable binary

--- host/install.bat
+++ host/install.bat
@@ -3,7 +3,11 @@
 SET NAME=com.clear_code.ieview_we_host
 SET INSTALL_DIR=%LocalAppData%\%NAME%
 MD "%INSTALL_DIR%"
-COPY *.exe "%INSTALL_DIR%\"
+IF %PROCESSOR_ARCHITECTURE% == AMD64 (
+  COPY amd64\*.exe "%INSTALL_DIR%\"
+) ELSE (
+  COPY 386\*.exe "%INSTALL_DIR%\"
+)
 COPY *.json "%INSTALL_DIR%\"
 COPY *.bat "%INSTALL_DIR%\"
 
