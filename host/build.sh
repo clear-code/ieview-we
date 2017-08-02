99bb0b97be16bae22ed18af26de22f8cea57f9b4
YUKI Hiroshi <shimoda@clear-code.com>
2017-08-02 11:13:46 +0900

    Make buildscript more robust

--- host/build.sh
+++ host/build.sh
@@ -1,18 +1,49 @@
 #!/usr/bin/env bash
 
+#set -x
+
 dist_dir="$(cd "$(dirname "$0")" && pwd)"
+temp_src="src/temp_ieview_we"
 
-cd "$GOHOME"
+if [ "$GOPATH" = '' ]
+then
+  echo 'ERROR: You must set GOPATH environment variable before you run this script.' 1>&2
+  exit 1
+fi
 
-[ -d src/github.com/clear-code/ieview-we ] || go get github.com/clear-code/ieview-we
-[ -d src/golang.org/x/sys/windows/registry ] || go get golang.org/x/sys/windows/registry
-[ -d src/github.com/lhside/chrome-go ] || go get github.com/lhside/chrome-go
+if [ -d "$temp_src" ]
+then
+  echo "ERROR: You must remove previous '$temp_src' before building." 1>&2
+  exit 1
+fi
 
-GOOS=windows GOARCH=386 go build github.com/clear-code/ieview-we/host
-mkdir -p "$dist_dir/386"
-mv host.exe "$dist_dir/386/"
+main() {
+  cd "$GOPATH"
 
-GOOS=windows GOARCH=amd64 go build github.com/clear-code/ieview-we/host
-mkdir -p "$dist_dir/amd64"
-mv host.exe "$dist_dir/amd64/"
+  # prepare_dependency github.com/clear-code/ieview-we
+  mkdir -p "$temp_src"
+  ln -s "$dist_dir" "$temp_src/host"
+  prepare_dependency golang.org/x/sys/windows/registry
+  prepare_dependency github.com/lhside/chrome-go
 
+  build_for 386
+  build_for amd64
+
+  rm "$temp_src/host"
+  rm -rf "$temp_src"
+}
+
+prepare_dependency() {
+  local path="$1"
+  [ -d "src/$path" ] || go get "$path"
+}
+
+build_for() {
+  local arch="$1"
+  local path="$(echo "$temp_src" | sed 's;^src/;;')/host"
+  env GOOS=windows GOARCH="$1" go build "$path"
+  mkdir -p "$dist_dir/$arch"
+  mv host.exe "$dist_dir/$arch/"
+}
+
+main
