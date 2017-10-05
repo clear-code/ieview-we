dccf0114307f8c28bcc5ec3949c7aeb43bac5546
YUKI Hiroshi <shimoda@clear-code.com>
2017-10-05 19:04:51 +0900

    Add ability to install managed storage globally

--- managed-storage/install.bat
+++ managed-storage/install.bat
@@ -4,13 +4,26 @@ SET NAME=ieview-we@clear-code.com
 
 ECHO Installing %NAME%...
 
-SET INSTALL_DIR=%LocalAppData%\%NAME%
+ECHO Checking permission...
+SET INSTALL_DIR=%ProgramFiles%\%NAME%
+SET REG_BASE=HKLM
+MD "%INSTALL_DIR%_try"
+IF EXIST "%INSTALL_DIR%_try\" (
+  ECHO Install for all users
+  RMDIR /Q /S "%INSTALL_DIR%_try"
+) ELSE (
+  ECHO Install for the current user
+  SET INSTALL_DIR=%LocalAppData%\%NAME%
+  SET REG_BASE=HKCU
+)
+
 MD "%INSTALL_DIR%"
+CD /D %~dp0
 COPY *.json "%INSTALL_DIR%\"
 
 ECHO Registering...
 FOR %%f IN ("%INSTALL_DIR%") DO SET EXPANDED_PATH=%%~sf
-REG ADD "HKCU\SOFTWARE\Mozilla\ManagedStorage\%NAME%" /ve /t REG_SZ /d "%EXPANDED_PATH%\%NAME%.json" /f
+REG ADD "%REG_BASE%\SOFTWARE\Mozilla\ManagedStorage\%NAME%" /ve /t REG_SZ /d "%EXPANDED_PATH%\%NAME%.json" /f
 
 ECHO Done.
 PAUSE
--- managed-storage/uninstall.bat
+++ managed-storage/uninstall.bat
@@ -4,9 +4,22 @@ SET NAME=ieview-we@clear-code.com
 
 ECHO Uninstalling managed storage for %NAME%...
 
-REG DELETE "HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\%NAME%" /f
+ECHO Checking permission...
+SET INSTALL_DIR=%ProgramFiles%\%NAME%
+SET REG_BASE=HKLM
+MD "%INSTALL_DIR%_try"
+IF EXIST "%INSTALL_DIR%_try\" (
+  ECHO Uninstall for all users
+  RMDIR /Q /S "%INSTALL_DIR%_try"
+) ELSE (
+  ECHO Uninstall for the current user
+  SET INSTALL_DIR=%LocalAppData%\%NAME%
+  SET REG_BASE=HKCU
+)
 
-RMDIR /Q /S "%LocalAppData%\%NAME%"
+REG DELETE "%REG_BASE%\SOFTWARE\Mozilla\NativeMessagingHosts\%NAME%" /f
+
+RMDIR /Q /S "%INSTALL_DIR%"
 
 ECHO Done.
 PAUSE
